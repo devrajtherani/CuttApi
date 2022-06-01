8c52c77f24eb9524affcd76ec95f30e8bd0eac21
Devraj Therani <83823130+devrajtherani@users.noreply.github.com>
2022-06-01 19:44:20 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from distutils.core import setup
+from setuptools import setup
 setup(
   name = 'CuttApi',         # How you named your package folder (MyLib)
   packages = ['CuttApi'],   # Chose the same as "name"
