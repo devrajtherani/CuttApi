5a9b9148a78f9cf6a074f0f6b9da146fa3cfedc5
Devraj Therani <83823130+devrajtherani@users.noreply.github.com>
2022-06-09 15:57:56 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup
 setup(
   name = 'CuttApi',         
   packages = ['CuttApi'],   
-  version = '0.1.2',      
+  version = '0.1.3',      
   license='MIT',        
   description = 'One of the best ways of shortening URLs using Cuttly URL shortener just by entering your API key and seeing the rest that is done automatically by the Module',   
   author = 'Devraj Therani',                   
   author_email = 'ttctlc96e@mozmail.com',      
   url = 'https://github.com/devrajtherani/CuttApi',   
-  download_url = 'https://github.com/devrajtherani/CuttApi/archive/refs/tags/v_0.1.2.tar.gz',    
+  download_url = 'https://github.com/devrajtherani/CuttApi/archive/refs/tags/v_0.1.3.tar.gz',    
   keywords = ['API', 'SIMPLE'],   
   install_requires=[            
           'requests',
